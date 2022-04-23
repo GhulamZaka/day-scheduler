a94de32eaee5b4c295397ed1c0883015e295c078
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-04-23 12:52:13 -0700

    adding past,present, and future display style

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,31 @@
 
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-    //display current day & time.
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-})
+// variables for date and hour
+var currentHour = moment().hour();
+var militaryHour = [9,10,11,12,13,14,15,16,17];
+console.log(currentHour);
+
+// display day and date
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+
+// display past, present and future hours 
+for (var i = 0; i < militaryHour.length; i++) {
+    if (currentHour === militaryHour[i]) {
+        console.log(i);
+        document.getElementById("hour" + militaryHour[i]).classList.add("present");
+    }
+    if (currentHour > militaryHour[i]) {
+        console.log(i);
+        document.getElementById("hour" + militaryHour[i]).classList.add("past");
+    }
+    if (currentHour < militaryHour[i]) {
+        console.log(i);
+        document.getElementById("hour" + militaryHour[i]).classList.add("future");
+    }
+    
+};
+
+
+
+
+// saving data into local storage 
