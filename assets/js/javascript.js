9df3d119e8cfdeefe173cc93ee03f5f8ad40781a
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-04-23 10:00:44 -0700

    displaying day and date

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,20 +1,5 @@
 
-var hour = [8,9,10,11,12,1,2,3,4,5];
-
-var currentHour = moment().hours();
-console.log(currentHour);
-
-var currentDay = document.querySelector("#currentDay");
-currentDay.textContent = moment().format("dddd, MMMM Do YYYY");
-
-
-for (var i =0; i < 9; i++) {
-    if  (currentHour === hour[i]) {
-        console.log(i);
-        document.getElementById(i).classList.add("present");
-    }
-    if (currentHour > hour[i]) {
-        console.log(i);
-        document.getElementById(i).classList.add("past");
-    }
-}
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+})
