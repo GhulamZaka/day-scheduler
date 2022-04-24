b677b3dc8de34804d2fc0ebbe932eae6be4b509b
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-04-23 23:40:13 -0700

    saving data in local storage and completing ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # day-scheduler
+
+## Description
+
+- This daily planner has 8 rows of block-time for each business hour on the day.
+- It shows the current day and date at the top of the scheduler.
+- The past, present, and future hours of the time block are distingushed by different colors. 
+- Users can enter events in the time block and can save it to local storage. 
+- The events will persist in the browser until you delet it from local storage. 
+
+
+## The URL of the functional, deployed application
+
+https://ghulamzaka.github.io/day-scheduler/
+
+## The URL of the GitHub repository
+
+https://github.com/GhulamZaka/day-scheduler
+
+
+## Screenshoot
+
+<img src="./assets/image/aa.jpg" />
+
+
+
+
+
+
+
+
+
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,7 @@
 // variables for date and hour
 var currentHour = moment().hour();
 var militaryHour = [9,10,11,12,13,14,15,16,17];
+var saveBtn = document.querySelectorAll(".saveBtn");
 console.log(currentHour);
 
 // display day and date
@@ -10,6 +11,7 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 // display past, present and future hours 
 for (var i = 0; i < militaryHour.length; i++) {
+
     if (currentHour === militaryHour[i]) {
         console.log(i);
         document.getElementById("hour" + militaryHour[i]).classList.add("present");
@@ -22,10 +24,27 @@ for (var i = 0; i < militaryHour.length; i++) {
         console.log(i);
         document.getElementById("hour" + militaryHour[i]).classList.add("future");
     }
-    
+
+    // Saving data into local storage 
+    saveBtn[i].addEventListener("click",function(){
+    var id = this.getAttribute("data-id");
+    var text = document.getElementById(id).value;
+    localStorage.setItem(id,text);
+    console.log(text);
+    })
+
+    // Retrieving data from local storage
+    if(localStorage.getItem(i)){
+    document.getElementById(i).value = localStorage.getItem(i);
+    }   
 };
 
 
 
 
-// saving data into local storage 
+
+
+
+
+
+
